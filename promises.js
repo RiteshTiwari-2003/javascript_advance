d7a17d48161dd393b55671fb9aedc3c8a34356e1
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-21 00:36:38 +0530

    this is my eighty five commit

--- promises.js
+++ promises.js
@@ -23,8 +23,42 @@ createOrder(cart,function(orderId){
  * now this create order api a async operation it takes time but we dont know how much time it take,but as soon line 19 is executed it return a object.
  * 
  * with data with some empty undefiend property, it assume that it return some empty object with some undefined data, and after 3 second 4 second at whatever time this createorder async fiunction executed
- * it fill the value at the place of undefined
+ * it fill the value at the place of undefined, this promise data field automatically after the this async operation done, and at the undefined basically which is a empty data is replaced by 
+ * some data like orderdetails,
+ * once we have orderdetails how we continue the program forward, how to we proceed to payment.
+ * 
+ * now what will we do , we attach callback function to the promise object
+ * 
+ * 
+ * promise.then(function (orderId){
+ * proceedToPayment(orderId)});
+ * 
+ * 
+ * 
  * 
  * 
  */
+//and we can say that this peace of code is lot of better 
+const promise=createOrder(cart);
+promise.then(function (orderId){
+    proceedToPayment(orderId)}); // then 
+    //this function like
+    createOrder(cart,function(orderId){
+        proceedToPayment(orderId);
+    });
+
+/**in above case we attach the callback function to the promise object and in second one old one we were passes the callbback function to the another function 
+ * so there is difference between the word attaching and passing in old one we pass the callback funtion to the another function api,
+ * but in new one we attach the caqllback function in the promise object
+ * in old one proceedpayment dependent on the another function api which is create order api but in new one this control is taken by ourself.
+ * in new one with the help of promise the createorder apin the do only its job,
+ * and it will fill the promise object with the data whatever it return, and as soon as the promise data filled with data that returened by createorder function after that it immediate execute or automatically call the callback funcction or the proceedpayment api
+ * 
+ * and now control of program with us and promises gives this control to us and promises give us this trust and garantee that it will call this callback function whenever there is data inside the promise object
+ * and in earlier piece of code we have lot of doubts that what hve this createorder api might have call the function twice thrice and might never call it
+ * promise handle it peacefully
+ * in new one as soon as the data inside the promise it call the callback function and proceed payment definetly
+ */
+
+
 
