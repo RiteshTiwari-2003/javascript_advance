f5f99a33c4275657f28ea9eb8c2b59e6b6bb50d9
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-20 22:46:22 +0530

    this is eighty two commit

--- promises.js
+++ promises.js
@@ -1,1 +1,15 @@
 /**promises are uses to handle asynchronus operation in javascript */
+const cart=["shoes","pants","kurta"];
+//createOrder(cart);//create order api return orderId
+//proceedToPayment(orderId);//this  is executed after create order api
+/**so now check how we write this code using callback using callback we bring asynchronous functioon and property in code
+ * 
+ */
+createOrder(cart,function(){
+    proceedToPayment(orderId);
+});
+/**now this is responsibility of crweate order api to create order and execute or call the callback function after that
+ * but in this very importent issue we faces which is inversion of control
+ * 
+ */
+
