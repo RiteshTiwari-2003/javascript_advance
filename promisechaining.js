9fd27c0c6174003625e4d8d8ae3a202f0ee00950
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-21 13:31:32 +0530

    this is my ninty one commit

--- promisechaining.js
+++ promisechaining.js
@@ -3,3 +3,6 @@
  * live example of promise chaining
  */
 const cart=["shoes","pants","kurta"];
+const promise=createOrder(cart);
+promise.then(function(orderId){proceedToPayment(orderId);})
+/**how does we return promise from createorder api then we have knowledge about thaty how we create order and send or return the pronise to the consumer */
