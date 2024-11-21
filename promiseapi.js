4309d8a0e8a5e2396f81fdd81bc8f8eafd089226
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-21 18:50:40 +0530

    this is my ninty five commit

---
+++ promiseapi.js
@@ -0,0 +1,8 @@
+/**in this article we going to know about all promise api
+ * so first promise api that we are going to learn is 
+ * promise all()
+ * so why this promise.all is used and what is the contratc for it me tell you that suppose you have to make parallel api call and get the result it is very
+ * importent use case very populer use case 
+ * what happen lets suppose you have 10 ids let me taken hypothetical example there are 10 ids call it ass user ids and you want you have api call for user info and 
+ * you want to make 10 
+ */
